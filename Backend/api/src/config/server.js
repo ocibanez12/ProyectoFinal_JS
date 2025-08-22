c5b1dafd764ee70597bdfbafc53bb41b19bf0730
ocibanez12 <octavio.ibanez.abarca@gmail.com>
2025-08-22 00:42:07 -0400

    commit master

--- Backend/api/src/config/server.js
+++ Backend/api/src/config/server.js
@@ -2,7 +2,7 @@
 export const serverConfig = {
   port: process.env.PORT || 3000,
   cors: {
-    origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+    origin: process.env.CORS_ORIGIN || 'https://proyectofinal-front.onrender.com',
     credentials: true
   }
 };
