cd947bfb51d3f63b7178a663a5d28110171bb8ce
ocibanez12 <octavio.ibanez.abarca@gmail.com>
2025-08-22 01:46:05 -0400

    update server.js

--- Backend/api/src/config/server.js
+++ Backend/api/src/config/server.js
@@ -2,7 +2,11 @@
 export const serverConfig = {
   port: process.env.PORT || 3000,
   cors: {
-    origin: process.env.CORS_ORIGIN || 'https://proyectofinal-front.onrender.com',
+    origin: process.env.CORS_ORIGIN || [
+      'https://proyectofinal-front.onrender.com',
+      'https://proyecto-final-js-six-dun.vercel.app',
+      'http://localhost:5173' // Para desarrollo local
+    ],
     credentials: true
   }
 };
